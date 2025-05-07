72f8ce3fb2ecaf15f39b99f7cac32859a6cec27d
JuniorVicente07 <jecaplayer07@gmail.com>
2025-05-07 20:45:12 -0300

    Add files via upload

---
+++ Simuladoragua.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simuladoragua;
+
+/**
+ *
+ * @author Victória
+ */
+public class Simuladoragua {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
